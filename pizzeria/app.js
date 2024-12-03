e61695841d37bc40cc6111ffa503080a2add7623
Guiboy974 <139962386+Guiboy974@users.noreply.github.com>
2024-12-03 09:28:42 +0100

    select ok

--- pizzeria/app.js
+++ pizzeria/app.js
@@ -1,9 +1,11 @@
 "use strict"
 
 const listPizza = document.getElementById("list-pizza");
-const filtre = document.getElementById("select-filtre")
+const filtre = document.getElementById("select-filtre");
+const commande = document.getElementById("commande");
+const container = document.getElementById("main");
 
-// recupère les json des pizzas
+// recupère et utilise les données json des pizzas
 async function recuperePizza() {
     try {
         const listJson = await Promise.all([
@@ -22,22 +24,32 @@ async function recuperePizza() {
 
         // filtre les pizzas
         function filtrerPizza(event) {
-            listPizza.innerHTML = "";
-            for (let i = 0; i < data.length; i++) {
-
-                if (event.target.value == 1 && data[i].base === "tomate") {
-                    displayPizza(data)                  
+            const optionValue = event.target.value;
+            const pizzasFiltrees = data.filter(pizza => {
+                switch (optionValue) {
+                    case "1":
+                        return pizza.base === "tomate";
+                    case "2":
+                        return pizza.base === "crème";
+                    default:
+                        return true; // affiche toutes les pizzas
                 }
-
-                if (event.target.value == 2 && data[i].base === "crème") {
-                    displayPizza(data);
-                }
-                displayPizza(data);
-            }
+            });
+            displayPizzas(pizzasFiltrees);
         }
-        // displayPizza(data);
-        filtre.addEventListener("change", filtrerPizza);
 
+        // affiche les pizzas
+        function displayPizzas(pizzas) {
+            listPizza.innerHTML = "";
+            pizzas.forEach(pizza => displayPizza(pizza));
+        }
+
+        filtre.addEventListener("change", filtrerPizza);
+        listPizza.addEventListener("click", addPizzas);
+
+
+        //affichage initiale des pizza
+        data.forEach(data => displayPizza(data));
     } catch (error) {
         console.log(error);
     }
@@ -46,40 +58,55 @@ recuperePizza()
 
 // affiche less pizzas
 function displayPizza(data) {
-    for (let i = 0; i < data.length; i++) {
-        const liPizza = document.createElement("li");
-        const imgPizza = document.createElement("img");
-        const divText = document.createElement("div");
-        const divCount = document.createElement("div");
-        const nomPizza = document.createElement("h4");
-        const ingredients = document.createElement("p");
-        const prix = document.createElement("p");
-        const pCount = document.createElement("p");
+    const liPizza = document.createElement("li");
+    const imgPizza = document.createElement("img");
+    const divText = document.createElement("div");
+    const divCount = document.createElement("div");
+    const nomPizza = document.createElement("h4");
+    const ingredients = document.createElement("p");
+    const prix = document.createElement("p");
+    const pCount = document.createElement("p");
 
-        liPizza.classList.add("list-group-item", "list-group-item-success", "rounded", "p-2", "m-1", "d-flex", "position-relative", "align-sm-items-center");
-        imgPizza.classList.add("p-1", "mt-2", "w-25", "h-25");
-        imgPizza.setAttribute("src", data[i].image);
-        imgPizza.setAttribute("alt", `pizza ${data[i].nom}`);
-        divText.classList.add("mt-2", "mx-sm-2");
-        divCount.classList.add("fs-5", "fw-semibold", "position-absolute", "bottom-0", "end-0");
-        nomPizza.classList.add("m-0");
-        nomPizza.textContent = data[i].nom;
-        ingredients.classList.add("m-0", "fs-6", "text-wrap");
-        ingredients.textContent = data[i].ingredients;
-        prix.classList.add("mb-4", "fw-semibold");
-        prix.textContent = `Prix : ${data[i].prix} €`;
-        pCount.innerHTML = '<p class="d-flex p-sm-2 m-1"><i class="bi bi-dash-circle mx-1"></i><span class="count"></span><i class="bi bi-plus-circle mx-1"></i></p>';
+    liPizza.classList.add("list-group-item", "list-group-item-success", "rounded", "p-2", "m-1", "d-flex", "position-relative", "align-sm-items-center");
+    imgPizza.classList.add("p-1", "mt-2", "w-25", "h-25");
+    imgPizza.setAttribute("src", data.image);
+    imgPizza.setAttribute("alt", `pizza ${data.nom}`);
+    divText.classList.add("mt-2", "mx-sm-2");
+    divCount.classList.add("fs-5", "fw-semibold", "position-absolute", "bottom-0", "end-0");
+    nomPizza.classList.add("m-0");
+    nomPizza.textContent = data.nom;
+    ingredients.classList.add("m-0", "fs-6", "text-wrap");
+    ingredients.textContent = data.ingredients;
+    prix.classList.add("mb-4", "fw-semibold");
+    prix.textContent = `Prix : ${data.prix} €`;
+    pCount.innerHTML = '<p class="d-flex p-sm-2 m-1"><i class="bi bi-dash-circle mx-1"></i><span class="count"></span><i class="bi bi-plus-circle mx-1"></i></p>';
 
-        listPizza.appendChild(liPizza);
-        liPizza.appendChild(imgPizza);
-        liPizza.appendChild(divText);
-        liPizza.appendChild(divCount);
-        divText.appendChild(nomPizza);
-        divText.appendChild(ingredients);
-        divText.appendChild(prix);
-        divCount.appendChild(pCount);
-    }
+    listPizza.appendChild(liPizza);
+    liPizza.appendChild(imgPizza);
+    liPizza.appendChild(divText);
+    liPizza.appendChild(divCount);
+    divText.appendChild(nomPizza);
+    divText.appendChild(ingredients);
+    divText.appendChild(prix);
+    divCount.appendChild(pCount);
 }
 
 
+//ajoute les pizzas a commande
+// function addPizzas(event) {
+//     data.forEach(data => {
+//         const count = document.getElementsByClassName("count");
+//         count.textContent = 0;
+//         if (event.target.contains(".bi-plus-circle")) {
+//             count++;
+//             console.log(event.target);
 
+//         }
+//     });
+// }
+
+// affiche commande en cours
+function afficheCommande() {
+
+}
+commande.addEventListener("click", afficheCommande);
