7346b14c95dd5141c56266254bea8d837b659733
Guiboy974 <139962386+Guiboy974@users.noreply.github.com>
2024-12-06 10:57:35 +0100

    cuisine - README -import

--- cuisine_pizzeria/cuisine.js
+++ cuisine_pizzeria/cuisine.js
@@ -16,8 +16,7 @@ const commandeClient = await recupereCommande();
 console.log(commandeClient);
 
 const enPreparation = document.getElementById("preparation");
-const enLivraison = document.getElementById("livraison")
-const aEmporter = document.getElementById("emporte")
+const prete = document.getElementById("prete")
 
 function afficheCommande(data) {
     const divCard = document.createElement("div");
@@ -27,7 +26,7 @@ function afficheCommande(data) {
     const cardAdresse = document.createElement("p");
     const cardPizza = document.createElement("p");
     const idCard = document.createElement("p");
-    const btnEtat = document.createElement("btn");
+    const btnEtat = document.createElement("button");
 
     divCard.classList.add("card", "col-md-4", "mb-3");
     cardBody.classList.add("card-body");
@@ -57,7 +56,6 @@ function afficheCommande(data) {
             data.commande.forEach(commande => {
                 const nomPizza = `${commande.nom} x ${commande.nombre}`
                 nomsPizza.push(nomPizza)
-                console.log(nomPizza);
             })
             cardPizza.textContent = nomsPizza.join(" , ")
         }
@@ -66,7 +64,7 @@ function afficheCommande(data) {
         }
     }
 
-    
+
     enPreparation.appendChild(divCard)
     divCard.appendChild(cardBody)
     cardBody.appendChild(nomClient)
@@ -77,21 +75,83 @@ function afficheCommande(data) {
     cardBody.appendChild(btnEtat)
 }
 
-commandeClient.forEach(element => { 
+commandeClient.forEach(element => {
     afficheCommande(element);
- });
+});
 
 //change l'état de la commande de "en préparation a prête" et les passes dans le section correspondante
 function changerEtat(event) {
     if (event.target.tagName === "BUTTON") {
-        for (let i = 0; i < commandeClient.length; i++) {     
-            if(event.target.previousSibling.textContent == commandeClient[i].id ){
-                console.log(event.target, commandeClient[i].id);
-                
+        for (let i = 0; i < commandeClient.length; i++) {
+
+            if (event.target.previousSibling.textContent == commandeClient[i].id) {
+
+                const divCard = document.createElement("div");
+                const cardBody = document.createElement("div");
+                const nomClient = document.createElement("h4");
+                const phoneClient = document.createElement("p");
+                const cardAdresse = document.createElement("p");
+                const cardPizza = document.createElement("p");
+                const idCard = document.createElement("p");
+                const btnEtat = document.createElement("button");
+
+                divCard.classList.add("card", "col-md-4", "mb-3");
+                cardBody.classList.add("card-body");
+                nomClient.classList.add("card-title");
+                phoneClient.classList.add("card-text", "fw-semibold");
+                cardAdresse.classList.add("card-text");
+                cardPizza.classList.add("card-text");
+                idCard.classList.add("card-text");
+                btnEtat.classList.add("btn", "btn-outline-success");
+                btnEtat.textContent = "Terminer"
+
+                for (const key in commandeClient[i]) {
+                    if (commandeClient[i].hasOwnProperty(key) && key === "client") {
+                        nomClient.textContent = commandeClient[i].client;
+                    }
+                    if (commandeClient[i].hasOwnProperty(key) && key === "telephone") {
+                        phoneClient.textContent = commandeClient[i].telephone;
+                    }
+                    if (commandeClient[i].hasOwnProperty(key) && key === "adresse") {
+                        cardAdresse.textContent = commandeClient[i].adresse;
+                    }
+                    if (commandeClient[i].hasOwnProperty(key) && key === "adresseZip") {
+                        cardAdresse.textContent = `${cardAdresse.textContent}, ${commandeClient[i].adresseZip}`;
+                    }
+                    if (commandeClient[i].hasOwnProperty(key) && key === "commande") {
+                        const nomsPizza = [];
+                        commandeClient[i].commande.forEach(commande => {
+                            const nomPizza = `${commande.nom} x ${commande.nombre}`
+                            nomsPizza.push(nomPizza)
+                        })
+                        cardPizza.textContent = nomsPizza.join(" , ")
+                    }
+                    if (commandeClient[i].hasOwnProperty(key) && key === "id") {
+                        idCard.textContent = commandeClient[i].id;
+                    }
+                }
+
+                prete.appendChild(divCard)
+                divCard.appendChild(cardBody)
+                cardBody.appendChild(nomClient)
+                cardBody.appendChild(phoneClient)
+                cardBody.appendChild(cardAdresse)
+                cardBody.appendChild(cardPizza)
+                cardBody.appendChild(idCard)
+                cardBody.appendChild(btnEtat)
             }
+
         }
+        event.target.parentElement.parentElement.remove();
+    }
+}
+
+//terminer au click lorsque la commande a été récupérer ou livré
+function terminerCommande(event) {
+    if (event.target.tagName === "BUTTON") {
+            event.target.parentElement.parentElement.remove();
     }
 }
 
 enPreparation.addEventListener("click", changerEtat);
-
+prete.addEventListener("click", terminerCommande)
