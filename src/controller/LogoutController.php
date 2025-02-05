6099ee7da93e0a8d8aa286529ce335828ca29b24
Guiboy974 <139962386+Guiboy974@users.noreply.github.com>
2025-02-05 16:52:46 +0100

    enhance LogoutController to clear session data and cookies on logout

--- src/controller/LogoutController.php
+++ src/controller/LogoutController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\controller;
 
 use App\controller\ControllerInterface;
@@ -16,8 +17,16 @@ class LogoutController implements ControllerInterface {
          */
         public function doGet(){
             session_start();
+            $_SESSION = array();
+            if (ini_get("session.use_cookies")) {
+                $params = session_get_cookie_params();
+                setcookie(session_name(), '', time() - 42000,
+                    $params["path"], $params["domain"],
+                    $params["secure"], $params["httponly"]
+                );
+            }
             session_destroy();
-            echo '<h2>Vous êtes déconnecté</h2>';
             header('refresh:1;url=index.php');
+            echo '<h2>Vous êtes déconnecté</h2>';
         }  
     }
